2ac2e02d142d2591e399118d9742b4e1774c89e6
Aswinellath <aswin@midnay.com>
2026-03-12 18:25:28 +0530

    Updates on POM

--- my-first-playwright-project/playwright.config.ts
+++ my-first-playwright-project/playwright.config.ts
@@ -4,9 +4,11 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-// import path from 'path';
-dotenv.config();
+// import dotenv from 'dotenv';
+
+
+// // import path from 'path';
+// dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -23,30 +25,41 @@ dotenv.config();
 
 //   return urls[site] || urls.saucedemo;
 // };
+
 export default defineConfig({
+
+  // Test directory
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],['list'],['json', { outputFile: 'test-results.json' }]],
+
   /* Global test timout */
   timeout: 30 * 1000,
+
   // Global expected timout
   expect: {
     timeout: 5000
   },
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: process.env.BASE_URL || 'http://www.saucedemo.com',
     // baseURL: process.env.BASE_URL || 'https://www.saucedemo.com',
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'https://www.saucedemo.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
